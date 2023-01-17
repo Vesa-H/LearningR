9f46d2b6e7b8831e3a03915952a07928766bc1df
Vesa Halipi <vesa.halipi@chalmers.se>
2023-01-17 11:15:58 +0100

    Made smaller nhanes dataset

--- R/learning.R
+++ R/learning.R
@@ -1,9 +1,34 @@
-#Here's an example of a conflict.
-2 + 2
-
+# Here's an example of a conflict.
 
 # Packages ----------------------------------------------------------------
 
 library(tidyverse)
+library(NHANES)
 
 # This will be used for testing out Git
+
+
+# Looking at data ---------------------------------------------------------
+
+glimpse(NHANES)
+
+select(NHANES, Age, Weight)
+
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"))
+select(NHANES, ends_with("Day"))
+select(NHANES, contains("Age"))
+
+nhanes_small <- select(
+  NHANES,
+  Age,
+  Gender,
+  BMI,
+  Diabetes,
+  PhysActive,
+  BPSysAve,
+  BPDiaAve,
+  Education
+)
+nhanes_small
